1c55988b54c82256b4cfc03e0eee464080231cfb
HelioAnacronista <heliojobs25@gmail.com>
2022-09-23 16:47:00 -0300

    ORM - ManyToMany

---
+++ src/main/java/github/helioanacronista/dscommerce/entities/Category.java
@@ -0,0 +1,19 @@
+package github.helioanacronista.dscommerce.entities;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
+}
