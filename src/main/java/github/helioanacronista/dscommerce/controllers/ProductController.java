c42101205327582e97fe4da62efc9c7960ddd3de
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-09-29 18:48:28 -0300

    fix : PUT correção do método update do controllerProduct

--- src/main/java/github/helioanacronista/dscommerce/controllers/ProductController.java
+++ src/main/java/github/helioanacronista/dscommerce/controllers/ProductController.java
@@ -44,7 +44,7 @@ public class ProductController {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> findById(@PathVariable Long id, @RequestBody ProductDTO dto) {
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
         dto = productService.update(id, dto);
         return ResponseEntity.ok(dto);
     }
